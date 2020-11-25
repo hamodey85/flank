6a1441707d2dba6173115af48fb3417adf6a4d56
Mohammed Almajid <hamodey85@gmail.com>
2020-11-25 22:27:51 +1100

    adding arrow

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 window.scrollBy({
-    top: 1000, // could be negative value
+    top: 1, // could be negative value
     left: 0,
     behavior: 'smooth'
   });
+  var arrow = document.getElementById("arrow");
+  var sentence_container = document.getElementById("sentence-container");
+  arrow.addEventListener('click', ()=>{
+    sentence_container.scrollIntoView({behavior: "smooth", block: "start"});
+  })
