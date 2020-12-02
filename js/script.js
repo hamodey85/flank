c1417ce08c8cbbbc5e454fd7fb9640b57667bd26
Mohammed Almajid <hamodey85@gmail.com>
2020-12-02 20:02:23 +1100

    Co-authored-by: Amelia lim <limamelia457@hotmail.com>

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// window.scroll({
+//     top: 1,
+//     left: 1,
+//     behavior: 'smooth'
+//   });
+//   window.addEventListener("", function(e){
+
+//     e.preventDefault();
+//     console.log(e);
+
+// } );
+// window.addEventListener("scroll", function(e){
+
+//     e.preventDefault();
+//     console.log(e);
+
+// } );
