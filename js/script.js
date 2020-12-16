d3b17d723af79cd5273a3a191039c95589d48b30
Mohammed Almajid <hamodey85@gmail.com>
2020-12-16 17:24:47 +1100

    mobile desgin huge change

--- js/script.js
+++ js/script.js
@@ -1,17 +1,22 @@
-// window.scroll({
-//     top: 1,
-//     left: 1,
-//     behavior: 'smooth'
-//   });
-//   window.addEventListener("", function(e){
+const flankText = document.getElementById('flank')
+const about = document.querySelector('.about')
+const about_flank_big = document.querySelector('.about_flank_big')
 
-//     e.preventDefault();
-//     console.log(e);
+flankText.addEventListener('mousedown',showPage)
+flankText.addEventListener('touchstart',showPage)
+flankText.addEventListener('touchend',up)
 
-// } );
-// window.addEventListener("scroll", function(e){
+flankText.addEventListener('mouseup',up)
 
-//     e.preventDefault();
-//     console.log(e);
+function showPage(e){
 
-// } );
+    about.classList.remove("hidden")
+    about.classList.add("visible")
+    about_flank_big.classList.add("about_flank_big_animated")
+}
+function up(e){
+    about.classList.remove("visible")
+    about.classList.add("hidden")
+    about_flank_big.classList.remove("about_flank_big_animated")
+
+}
